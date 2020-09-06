25ebe7ab797f441ce05fc2e155286fafe8c1b5e5
Anton Omelchuk <kino_kg@ukr.net>
2020-09-06 13:02:20 +0300

    implementation react render

--- todo-app.js
+++ todo-app.js
@@ -1,3 +1,11 @@
 /** @jsx React.createElement */
 
-const $root = document.getElementById('root')
+const root = document.getElementById('root')
+
+const App = React.createElement(
+  'h1',
+  {},
+  'React Lite Library'
+)
+
+React.render(App, root)
